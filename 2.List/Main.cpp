04909cdb22cf87b3bea9ec105e02a0956e957305
ronniej <ronniej@naver.com>
2026-02-13 10:09:03 +0900

    List / ListIterator 진행
    - List 기존 코드에서 문제 있는 부분 수정
    - ListIterator 만들고, 개념 설명
    - 포인터를 배열로 확인할 때 유용한 debug 구문 설명 (예: list.data, 10)

--- 2.List/Main.cpp
+++ 2.List/Main.cpp
@@ -10,5 +10,22 @@ int main()
 		list.Add(ix + 1);
 	}
 
+	// Range-based loop.
+	std::cout << "Range-based loop\n";
+	for (const int item : list)
+	{
+		std::cout << item << "\n";
+	}
+
+	// loop - Iterator.
+	//std::cout << "Iterator based loop\n";
+	//for (
+	//	List<int>::Iterator it = list.begin(); 
+	//	it != list.end(); 
+	//	++it)
+	//{
+	//	std::cout << *it << "\n";
+	//}
+
 	std::cin.get();
 }
