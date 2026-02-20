39beccb64b6c2cbc3885f44aaf6a284113a589ef
ronniej <ronniej@naver.com>
2026-02-20 16:00:50 +0900

    다중 트리 구현
    - 삭제/전위순회/트리 제거 함수 구현

--- 10.Tree/Main.cpp
+++ 10.Tree/Main.cpp
@@ -1,7 +1,13 @@
 #include "Container/Tree.h"
 
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
 int main()
 {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
 	Tree<int> tree(10);
 	tree.AddChild(10, 9);
 	tree.AddChild(10, 8);
@@ -13,6 +19,15 @@ int main()
 	if (tree.Find(6, outNode))
 	{
 		int number2 = 30;
+	}
+
+	// 전위 순회 테스트.
+	tree.PreorderTraversal();
+
+	// 삭제 테스트.
+	if (tree.Remove(9))
+	{
+		int number3 = 30;
 	}
 
 	int number = 10;
