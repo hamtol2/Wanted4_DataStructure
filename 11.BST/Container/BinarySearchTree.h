3f0785cb89c3e5d42d40f7d4f6d61d1a9b59f24d
ronniej <ronniej@naver.com>
2026-02-23 12:01:08 +0900

    삭제 재귀함수 구현 및 기능 테스트 진행
    - 삭제 재귀 함수 구현
    - SearchMinValue 함수 구현 (최소값 검색 함수)
    - 삭제 기능 테스트 (Main.cpp 에서 진행)

--- 11.BST/Container/BinarySearchTree.h
+++ 11.BST/Container/BinarySearchTree.h
@@ -48,8 +48,8 @@ public:
 	// 삭제.
 	bool DeleteNode(const T& deleteData)
 	{
-		// Todo: 재귀 삭제 함수 구현 후 호출.
-
+		// 재귀 삭제 함수 구현 후 호출.
+		return DeleteNodeRecursive(root, deleteData, root);
 	}
 
 	// 검색.
@@ -181,16 +181,74 @@ private:
 				// 여기에서는 2가지 방법이 가능.
 				// 1. 왼쪽 하위 트리에서 가장 큰 값의 노드를 대체.
 				// 2. 오른쪽 하위 트리에서 가장 작은 값의 노드를 대체.
-				// Todo: 부분 함수 구현.
-				// - 하위 노드에서 최소값 찾는 함수 구현.
+				// 하위 노드에서 최소값 찾는 함수 호출.
+				node->data = SearchMinValue(node->right)->data;
+
+				// 오른쪽 하위 트리에서 가장 작은 값의 노드를 삭제
+				// 및 정리.
+				DeleteNodeRecursive(
+					node->right, node->data, node->right
+				);
+
+				return true;
 			}
 
 			// 경우의 수3 - 둘 중에 하나만 있는 경우.
 			else
 			{
+				// 왼쪽 자손이 null -> 오른쪽 자손만 있는 경우.
+				if (!node->left)
+				{
+					// 오른쪽 자손 정보를 임시 저장.
+					Node<T>* right = node->right;
 
+					// 오른쪽 자손의 부모를 삭제할 노드의 부모로 설정.
+					right->parent = node->parent;
+
+					// 노드 제거.
+					delete node;
+
+					// 오른쪽 자손 정보 설정.
+					outNode = right;
+					return true;
+				}
+				// 왼쪽 자손이 있는 경우.
+				else if (!node->right)
+				{
+					// 왼쪽 자손 정보를 임시 저장.
+					Node<T>* left = node->left;
+
+					// 왼쪽 자손의 부모를 삭제할 노드의 부모로 설정.
+					left->parent = node->parent;
+
+					// 노드 제거.
+					delete node;
+
+					// 왼쪽 자손 정보 설정.
+					outNode = left;
+					return true;
+				}
 			}
 		}
+
+		// 오류.
+		outNode = nullptr;
+		return false;
+	}
+
+	// 최소값 검색 함수.
+	// node: 검색을 시작하는 노드.
+	Node<T>* SearchMinValue(Node<T>* node)
+	{
+		// 검색 시작.
+		while (node->left)
+		{
+			// 왼쪽 하위 노드로 이동.
+			node = node->left;
+		}
+
+		// 최소 값을 가진 노드 반환.
+		return node;
 	}
 
 	// 파괴 함수.
--- 11.BST/Main.cpp
+++ 11.BST/Main.cpp
@@ -7,10 +7,18 @@ int main()
 	BinarySearchTree<int> tree;
 
 	// ª¿‘.
-	tree.InsertNode(10);
 	tree.InsertNode(20);
+	tree.InsertNode(10);
 	tree.InsertNode(5);
 	tree.InsertNode(30);
+	tree.InsertNode(25);
+	tree.InsertNode(35);
+	tree.InsertNode(8);
+
+	// ªË¡¶.
+	tree.DeleteNode(30);
+	tree.DeleteNode(10);
+	tree.DeleteNode(8);
 
 	std::cin.get();
 }
